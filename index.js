282d218025fe70edf690aeef58cce618661a0964
gbarnett374 <gbarnett374@gmail.com>
2016-11-07 09:04:27 -0500

    White listed users and responses, added underscore to get random element of array

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Botkit = require('Botkit');
+const _ = require('underscore');
 
 const controller = Botkit.slackbot({
     debug: true
@@ -10,9 +11,21 @@ controller.spawn({
 
 controller.hears('.*',['ambient'], (bot, message) => {
     bot.api.users.info({user: message.user}, (error, response) => {
-        let {name, real_name} = response.user;
-        if (name === 'gbarnett') {
-            bot.reply(message, "Hello " + name);
+        const {name, real_name} = response.user;
+        // Will add these to redis. 
+        const responses = [
+        	'Yep yep thats right!', 
+        	'Cool!', 
+        	'Us RPG guys wish we can be full stack.',
+        	'Big Data!',
+        	'I want to learn some Python like Tara at the Orchard.'
+        ];
+        const names = ['dbush', 'seanscottking', 'estreske'];
+        if (names.includes(name)) {
+            bot.reply(message, _.sample(responses));
+        }
+        else if (name === 'baz') {
+        	bot.reply(message, 'Shut up Baz!');
         }
      });
 });
